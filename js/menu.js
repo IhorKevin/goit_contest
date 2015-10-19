e8ee8b398c30866acb8a62b65dfdb9523790ae15
IhorKevin <ihorkevin@gmail.com>
2015-10-20 02:28:38 +0300

    fixed header

--- js/menu.js
+++ js/menu.js
@@ -8,15 +8,18 @@
     function toggleMenu() {
         if(menuDom.classList.contains('header__menu_open')) {
             menuDom.classList.remove('header__menu_open');
+            document.body.classList.remove('body_no-scroll');
         }
         else {
             menuDom.classList.add('header__menu_open');
+            document.body.classList.add('body_no-scroll');
         }
     }
 
     function closeMenu(e) {
         if(e.target.tagName == 'A') {
             menuDom.classList.remove('header__menu_open');
+            document.body.classList.remove('body_no-scroll');
         }
     }
 })(window);
